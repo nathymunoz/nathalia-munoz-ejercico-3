2071f23f589b025f34763d7d5979e4a695218474
nathymunoz <1006106561@u.icesi.edu.co>
2023-03-01 07:49:28 -0500

    commit-2

---
+++ main.js
@@ -0,0 +1,47 @@
+const input = document.querySelector('.task-list');
+const successBtn = document.querySelector('.btn-success');
+const ul = document.querySelector("ul");
+
+successBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const text = input.value;
+
+    if (text !== "") {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.textContent = text;
+    
+        li.appendChild(p);        
+        li.appendChild(addCheckBtn());
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+
+    }  
+})
+
+function addCheckBtn() {
+    const checkList = document.createElement("input");
+    
+    checkList.setAttribute("type", "checkbox");
+    checkList.textContent = "X";
+    checkList.className = "btn-delete";
+  
+    return checkList;
+  }
+
+function addDeleteBtn() {
+    const deleteBtn = document.createElement("button");
+  
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn-delete";
+  
+    deleteBtn.addEventListener("click", (e) => {
+      const item = e.target.parentElement;
+      ul.removeChild(item);
+  
+      const items = document.querySelectorAll("li");
+    });
+  
+    return deleteBtn;
+  }
